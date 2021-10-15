cabc2833dfe08e43975359785e6ecfcb91c7a074
AliFQ <71616390+Ali-F-Q@users.noreply.github.com>
2021-10-15 12:20:42 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Main {
+  public static final String ANSI_RESET = "\u001B[0m";
+  public static final String ANSI_RED = "\u001B[31m";
+  public static void main(String[] args) {
+    Scanner rip = new Scanner(System.in);
+
+    System.out.print("How many rows: ");
+    int rows = rip.nextInt();
+    System.out.print("How many columns: ");
+    int columns = rip.nextInt();
+    int[][] multi = new int[rows][columns];
+    
+    System.out.println();
+    System.out.println("Multiplication Table");
+    System.out.print("|");
+    for(int x = 0;x < columns*5-1;x++){
+        System.out.print("-");
+    }
+    System.out.println("|");
+    
+
+    for(int i = 1;i < rows+1;i++){
+      System.out.print("|");
+
+      //Calculation
+      for(int y = 1;y < columns+1;y++){
+        multi[i-1][y-1] = i * y;
+        int sqrt = (int) Math.sqrt(multi[i-1][y-1]);/////////////
+        product = String.valueOf(multi[i-1][y-1]);////////////Integer.toString()
+
+        if(sqrt == (int) multi[i-1][y-1]){
+          System.out.printf("%4d%s",ANSI_RED + multi[i-1][y-1] + ANSI_RESET,"|");
+        }
+        else{
+          System.out.printf("%4d%s",multi[i-1][y-1],"|");
+        }
+      }
+
+      System.out.println();
+      System.out.print("|");
+      for(int x = 0;x < columns*5-1;x++){
+      System.out.print("-");
+      }
+      System.out.println("|");
+    }
+  }
+}
